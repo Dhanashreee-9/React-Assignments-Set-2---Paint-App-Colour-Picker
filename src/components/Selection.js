b35488fafbd36cbab8570623b0b85fcd0823797f
Dhanashree <dhanashreesarade919@gmail.com>
2021-05-21 20:52:11 +0530

    Added

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+import React,{useState} from 'react';
+import '../styles/Child.css';
 
+const Selection =({applyColor})=>{
+    const[backColor,setBackground] =useState({background:""});
+    return (
+       <div
+            className="fix-box"
+            style={backColor}
+            onClick={()=>{applyColor(setBackground);
+            }}>
+            <h1 className="subheading">{"Selection"}</h1>
+            </div>
+        
+    );
+};
+ 
+
+export default Selection;
